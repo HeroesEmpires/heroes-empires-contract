08cc390408d36de4d10ab5f822adf2d842145073
quyphandang <dangquyphan@gmail.com>
2022-01-04 10:35:46 +0700

    update stake

--- src/contracts/Stake.sol
+++ src/contracts/Stake.sol
@@ -1,15 +1,4 @@
-pragma solidity ^0.8.0;
-library Math {
-    function max(uint256 a, uint256 b) internal pure returns (uint256) {
-        return a >= b ? a : b;
-    }
-    function min(uint256 a, uint256 b) internal pure returns (uint256) {
-        return a < b ? a : b;
-    }
-    function average(uint256 a, uint256 b) internal pure returns (uint256) {
-        return (a / 2) + (b / 2) + ((a % 2 + b % 2) / 2);
-    }
-}
+pragma solidity 0.8.11;
 interface IBEP20 { 
 	function totalSupply() external view returns (uint256);
 	function balanceOf(address account) external view returns (uint256);
@@ -96,28 +85,6 @@ library Address {
 } 
 interface IERC165 {
     function supportsInterface(bytes4 interfaceId) external view returns (bool);
-}
-interface IERC721 is IERC165 {
-    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
-    event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
-    event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
-    function balanceOf(address owner) external view returns (uint256 balance);
-    function ownerOf(uint256 tokenId) external view returns (address owner);
-    function safeTransferFrom( address from, address to, uint256 tokenId) external;
-    function transferFrom( address from,address to,uint256 tokenId) external;
-    function approve(address to, uint256 tokenId) external;
-    function getApproved(uint256 tokenId) external view returns (address operator);
-    function setApprovalForAll(address operator, bool _approved) external;
-    function isApprovedForAll(address owner, address operator) external view returns (bool);
-    function safeTransferFrom( address from, address to, uint256 tokenId, bytes calldata data ) external;
-
-    struct HeroesInfo {uint256 heroesNumber; string name; string race; string class; string tier; string tierBasic; string uri;}
-    function getHeroesNumber(uint256 _tokenId) external view returns (HeroesInfo memory);
-    function safeMint(address _to, uint256 _tokenId) external;
-    function burn(address _from, uint256 _tokenId) external;
-    function addHeroesNumber(uint256 _tokenId, uint256 _heroesNumber, string memory name, string memory race, string memory class, string memory tier, string memory tierBasic) external;
-    function editTier(uint256 tokenId, string memory _tier) external;
-    function deleteHeroesNumber(uint256 tokenId) external;
 }
 abstract contract Context {
     function _msgSender() internal view virtual returns (address) { return msg.sender;}
@@ -259,7 +226,7 @@ contract Stake is AccessControl {
         uint256 _startBlock,
         uint256 _bonusEndBlock,
         address _reward
-    ) public {
+    ) {
         _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
         _setupRole(CREATOR_ADMIN, minter);
         HE = IBEP20(_HE);
@@ -272,15 +239,15 @@ contract Stake is AccessControl {
         return poolInfo.length;
     }
 
-    function changeTimeLockWithdraw(uint256 _timeLock) public {
+    function changeTimeLockWithdraw(uint256 _timeLock) external {
         require(hasRole(DEFAULT_ADMIN_ROLE, address(msg.sender)), "Caller is not a owner");
         timeLockWithdraw = _timeLock;
     }
-    function changeTimeLockClaim(uint256 _timeLock) public {
+    function changeTimeLockClaim(uint256 _timeLock) external {
         require(hasRole(DEFAULT_ADMIN_ROLE, address(msg.sender)), "Caller is not a owner");
         timeLockClaim = _timeLock;
     }
-    function changeHePerBlock(uint256 _hePerBlock) public {
+    function changeHePerBlock(uint256 _hePerBlock) external {
         require(hasRole(DEFAULT_ADMIN_ROLE, address(msg.sender)), "Caller is not a owner");
         massUpdatePools();
         HePerBlock = _hePerBlock;
@@ -291,7 +258,7 @@ contract Stake is AccessControl {
     function getClaimByAddress(uint256 _pid, address _address) public view returns (ClaimInfo[]  memory) {
         return claimInfo[_pid][_address];
     }
-    function add(uint256 _allocPoint, IBEP20 _heToken, bool _withUpdate) public {
+    function add(uint256 _allocPoint, IBEP20 _heToken, bool _withUpdate) external {
         require(hasRole(DEFAULT_ADMIN_ROLE, address(msg.sender)), "Caller is not a owner");
         require(block.number > startBlock, 'has not started yet');
         if (_withUpdate) {
@@ -307,7 +274,7 @@ contract Stake is AccessControl {
             balancePool: 0
         }));
     }
-    function set(uint256 _pid, uint256 _allocPoint, bool _withUpdate) public {
+    function set(uint256 _pid, uint256 _allocPoint, bool _withUpdate) external {
         require(hasRole(DEFAULT_ADMIN_ROLE, address(msg.sender)), "Caller is not a owner");
         if (_withUpdate) {
             massUpdatePools();
@@ -315,7 +282,7 @@ contract Stake is AccessControl {
         totalAllocPoint = totalAllocPoint.sub(poolInfo[_pid].allocPoint).add(_allocPoint);
         poolInfo[_pid].allocPoint = _allocPoint;
     }
-    function addViewWallet(uint256 _pid, address _receive) public{
+    function addViewWallet(uint256 _pid, address _receive) external {
         require(!boolAddWallet[_pid][_receive], "Wallet has been added");
         addressStake[_pid][address(_receive)] = msg.sender;
         boolAddWallet[_pid][_receive] = true;
@@ -348,7 +315,7 @@ contract Stake is AccessControl {
         }
     }
     // View function to see pending Lucky on frontend.
-    function pendingToken(uint256 _pid, address _user) external view returns (uint256) {
+    function pendingToken(uint256 _pid, address _user) public view returns (uint256) {
         PoolInfo storage pool = poolInfo[_pid];
         UserInfo storage user = userInfo[_pid][_user];
         uint256 accHePerShare = pool.accHePerShare;
@@ -388,7 +355,7 @@ contract Stake is AccessControl {
         pool.lastRewardBlock = block.number;
     }
     // Deposit HE tokens to MasterChef for HE allocation.
-    function deposit(uint256 _pid, uint256 _amount) public {
+    function deposit(uint256 _pid, uint256 _amount) external {
         PoolInfo storage pool = poolInfo[_pid];
         UserInfo storage user = userInfo[_pid][msg.sender];
         updatePool(_pid);
@@ -396,8 +363,6 @@ contract Stake is AccessControl {
             uint256 pending = user.amount.mul(pool.accHePerShare).div(1e18).sub(user.rewardDebt);
             if(pending > 0){
                 user.pendingDebt = user.pendingDebt.add(pending);
-                // claimInfo[_pid][msg.sender].push(ClaimInfo(pending, block.timestamp, 0)); 
-                // emit Claim(msg.sender, _pid, pending, pendingnfl, block.timestamp);
             }
         } 
         if(_amount > 0){
@@ -410,63 +375,57 @@ contract Stake is AccessControl {
     }
 
     // Withdraw HE tokens.
-    function pendingWithdraw(uint256 _pid, uint256 _amount) public {
+    function pendingWithdraw(uint256 _pid, uint256 _amount) external {
         PoolInfo storage pool = poolInfo[_pid];
         UserInfo storage user = userInfo[_pid][msg.sender];
         require(user.amount >= _amount, "withdraw: not good");
         updatePool(_pid);
         uint256 pending = user.amount.mul(pool.accHePerShare).div(1e18).sub(user.rewardDebt);
-        // safeHeTransfer(msg.sender, pending);
         user.amount = user.amount.sub(_amount);
         user.rewardDebt = user.amount.mul(pool.accHePerShare).div(1e18);
         pool.balancePool = pool.balancePool.sub(_amount);
         withdrawInfo[_pid][msg.sender].push(WithdrawInfo(_amount, block.timestamp, 0)); 
         user.pendingDebt = user.pendingDebt.add(pending);
-        // claimInfo[_pid][msg.sender].push(ClaimInfo(pending, block.timestamp, 0)); 
-        // emit Withdraw(msg.sender, _pid, _amount, block.timestamp);
-        // emit Claim(msg.sender, _pid, pending, block.timestamp);
     }
 
-    function withdraw(uint256 _pid, uint256 _id) public {
+    function withdraw(uint256 _pid, uint256 _id) external {
         PoolInfo storage pool = poolInfo[_pid];
         uint256 status = withdrawInfo[_pid][msg.sender][_id].status;
         uint256 amount = withdrawInfo[_pid][msg.sender][_id].amount;
         uint256 timeWithdraw = withdrawInfo[_pid][msg.sender][_id].blockTime;
         require(status == 0, 'You have withdrawn');
-        require(amount >= 0 , 'withdraw: not good');
+        // require(amount >= 0 , 'withdraw: not good');
         require((block.timestamp - timeWithdraw) > timeLockWithdraw, 'you are still in lock');
         withdrawInfo[_pid][msg.sender][_id].status = 1;
         pool.heToken.safeTransfer(address(msg.sender), amount);
         emit Withdraw(msg.sender, _pid, amount, block.timestamp);
     }
-    function pendingClaim(uint256 _pid, uint256 _amount) public {
+    function pendingClaim(uint256 _pid, uint256 _amount) external {
         PoolInfo storage pool = poolInfo[_pid];
         UserInfo storage user = userInfo[_pid][msg.sender];
-        require(user.amount >= 0, "withdraw: not good");
+        // require(user.amount >= 0, "withdraw: not good");
         updatePool(_pid);
         uint256 pending = user.amount.mul(pool.accHePerShare).div(1e18).sub(user.rewardDebt);
         user.rewardDebt = user.amount.mul(pool.accHePerShare).div(1e18);
         require(user.pendingDebt.add(pending) >= _amount, "Claim: not good");
         user.pendingDebt = user.pendingDebt.add(pending).sub(_amount);
         claimInfo[_pid][msg.sender].push(ClaimInfo(_amount, block.timestamp, 0)); 
-        // emit Claim(msg.sender, _pid, pending, block.timestamp);
     }
-    function claim(uint256 _pid, uint256 _id) public {
+    function claim(uint256 _pid, uint256 _id) external {
         uint256 status = claimInfo[_pid][msg.sender][_id].status;
         uint256 amount = claimInfo[_pid][msg.sender][_id].amount;
         uint256 timeWithdraw = claimInfo[_pid][msg.sender][_id].blockTime;
         require(status == 0, 'You have withdrawn');
-        require(amount >= 0 , 'withdraw: not good');
+        // require(amount >= 0 , 'withdraw: not good');
         require((block.timestamp - timeWithdraw) > timeLockClaim, 'you are still in lock');
         claimInfo[_pid][msg.sender][_id].status = 1;
-        // safeHeTransfer(address(msg.sender), amount);
         RE.transferToken(HE,address(msg.sender), amount);
         emit Claim(msg.sender, _pid, amount, block.timestamp);
     }
-    function reInvestment(uint256 _pid, uint256 _amount) public {
+    function reInvestment(uint256 _pid, uint256 _amount) external {
         PoolInfo storage pool = poolInfo[_pid];
         UserInfo storage user = userInfo[_pid][msg.sender];
-        require(user.amount >= 0, "amount: not good");
+        // require(user.amount >= 0, "amount: not good");
         require(pool.heToken == HE, "Token does not support restake");
         updatePool(_pid);
         uint256 pending = user.amount.mul(pool.accHePerShare).div(1e18).sub(user.rewardDebt);
@@ -477,13 +436,11 @@ contract Stake is AccessControl {
         user.rewardDebt = user.amount.mul(pool.accHePerShare).div(1e18);
         RE.transferToken(HE,address(this), _amount);
         emit ReInvestment(msg.sender, _pid, _amount, block.timestamp);
-        // claimInfo[_pid][msg.sender].push(ClaimInfo(_amount, block.timestamp, 0)); 
     }
     // Withdraw without caring about rewards. EMERGENCY ONLY.
-    function emergencyWithdraw(uint256 _pid) public {
+    function emergencyWithdraw(uint256 _pid) external {
         PoolInfo storage pool = poolInfo[_pid];
         UserInfo storage user = userInfo[_pid][msg.sender];
-        // pool.heToken.safeTransfer(address(msg.sender), user.amount);
         withdrawInfo[_pid][msg.sender].push(WithdrawInfo(user.amount, block.timestamp, 0));
         pool.balancePool = pool.balancePool.sub(user.amount);
         emit EmergencyWithdraw(msg.sender, _pid, user.amount, block.timestamp);
